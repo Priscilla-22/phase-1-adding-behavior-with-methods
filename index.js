72933ab4ee064d3cf4c4d9404460b999d85c3941
Priscilla-22 <priscillamw123@gmail.com>
2024-01-15 15:15:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+  }
+  sayName() {
+    return `${this.name} says meow!`;
+  }
+}
+const myCat = new Cat('Whiskers');
+
+class Dog {
+  constructor(name, sex) {
+    this.name = `${name}says woof!`;
+  }
+  sayName() {
+    return `${this.name}says woof!`;
+  }
+}
+const myDog = new Cat('Nora');
+
+class Bird {
+  constructor(name, sex) {
+    if (sex === 'male') {
+      this.speak=`It's me! ${name},the parrot!`;
+    } else {
+      this.speak=`${name} says squawk!`;
+    }
+  }
+    speakBird() {
+        return this.speak
+    }
+}
+const myBird = new Bird('Debby', 'male');
